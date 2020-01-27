c479d0b45c09d9d2c0246d85abf5da424d8fc3c2
Jeff Breig <jjbreig@gmail.com>
2020-01-26 21:24:14 -0500

    displaying player data

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
+class App extends React.Component {
+
+  state ={
+    players: []
+  };
+
+  
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/api/players")
+      .then(result => {
+        console.log(result);
+        this.setState({...this.state, players: result.data});
+      })
+      .catch(err=> console.log("error on get:", err));
+  }
+
+
+  render() {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="player-data">
+        {this.state.players.map(player => {
+          return (
+            <div className="player-card">
+              <p>{player.name}</p>
+              <p>{player.country}</p>
+              <p>{player.searches}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  );
-}
+    );
+  }
+} 
 
 export default App;
