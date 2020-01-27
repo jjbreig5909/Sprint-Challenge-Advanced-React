dc2a1b9af0f1dc0d74fd62fee15452118038400d
Jeff Breig <jjbreig@gmail.com>
2020-01-26 23:55:24 -0500

    kinda maybe got everything charted

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import axios from 'axios';
 import Navbar from './Components/Navbar';
+import { VictoryBar, VictoryChart, VictoryAxis } from "victory";
 
 class App extends React.Component {
 
@@ -23,20 +24,29 @@ class App extends React.Component {
 
   render() {
     return (
-    <div className="App">
-      <Navbar />
+      <div className="App">
+        <Navbar />
+        <div className="chart">
+          <VictoryChart>
+            <VictoryAxis
+              tickValues={[1, 25, 75, 100]}
+              tickFormat={["", "", "", ""]}
+            />
+            <VictoryBar data={this.state.players} x="name" y="searches" />
+          </VictoryChart>
+        </div>
         <div className="player-data" data-testid="player-data">
-        {this.state.players.map(player => {
-          return (
-            <div className="player-card" key={player.id}>
-              <p>Player: {player.name}</p>
-              <p>Country: {player.country}</p>
-              <p>Number of Searches: {player.searches}</p>
-            </div>
-          );
-        })}
+          {this.state.players.map(player => {
+            return (
+              <div className="player-card" key={player.id}>
+                <p>Player: {player.name}</p>
+                <p>Country: {player.country}</p>
+                <p>Number of Searches: {player.searches}</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div>
     );
   }
 } 
