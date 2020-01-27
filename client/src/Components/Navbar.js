9e5ce2b33283a6234e623185bf40ee8ef0fa4cc1
Jeff Breig <jjbreig@gmail.com>
2020-01-26 23:34:19 -0500

    animated dark mode toggle

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -11,11 +11,14 @@ const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>Women's World Cup Players</h1>
-            <div className="dark-mode__toggle">
+            <div className="dark-mode-info">
+            <h2>Toggle Dark Mode</h2>
+                <div className={darkMode ? "dark-mode__toggled" : "dark-mode__toggle"}>
                 <div
                     onClick={toggleMode}
                     className={darkMode ? 'toggle toggled' : 'toggle'}
-                />
+                    >{ darkMode? 'On': 'Off' }</div>
+            </div>
             </div>
         </nav>
     );
